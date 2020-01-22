a02619d49fe5aa79ce433d4deee4be9a06985d94
Roberto Esqueda <roberto.raeh@gmail.com>
2020-01-22 12:12:15 -0600

    Comments removed

--- app/Http/Controllers/Prueba/PruebaController.php
+++ app/Http/Controllers/Prueba/PruebaController.php
@@ -11,37 +11,36 @@ class PruebaController extends Controller
         $nombre = $request->input('nombre');
         $apellidos = $request->input('apellidos');
         $sexo = $request->input('sexo');
+        
         $alumnos = ['nombre' => $nombre, 'apellidos' => $apellidos, 'sexo' => $sexo];
         
         $request->session()->push('lista.alumnos', $alumnos);
         
-        // return $request->all();
-        // return view('folder.test')->with(['data' => $request->all()]);
         return back()->with(['data' => $request->all()]);
-        // $data = $request->session()->get('alumno');
     }
     
     public function delete(Request $request, $id) {
         $index = $id - 1;
+        
         session()->forget('lista.alumnos.'.$index);
+
         return back()->with(['data' => $request->all()]);
     }
     
     public function update(Request $request, $id) {
         $index = $id - 1;
+        
         $obj = $request->session()->get('lista.alumnos');
         
         $nombre = $request->input('nombre');
         $apellidos = $request->input('apellidos');
         $sexo = $request->input('sexo');
         
-        
         $obj[$index]['nombre'] = $nombre;
         $obj[$index]['apellidos'] = $apellidos;
         $obj[$index]['sexo'] = $sexo;
         
         session()->put('lista.alumnos', $obj);
-        // return $request->session()->all();
 
         return back()->with(['data' => $request->all()]);
     }
